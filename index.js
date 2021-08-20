03b2bc18e72a20261edf81ed55f8b9d8dc1723ed
Yeasin <yeasin30112001@gmail.com>
2021-08-20 15:50:50 -0600

    add port

--- index.js
+++ index.js
@@ -134,6 +134,4 @@ app.get("/", (req, res) => {
   res.send("photix server");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`);
-});
+app.listen(process.env.PORT || port);
