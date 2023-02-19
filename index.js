b318d86ff6dea07624d9b4229288bd325d3611bb
Yeasin <yeasin30112001@gmail.com>
2023-02-19 23:48:50 +0600

    mongoose added

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
 const express = require("express");
 const app = express();
-const port = 8080;
+const port = process.env.PORT;
 require("dotenv").config();
 const fs = require("fs-extra");
 const cors = require("cors");
 const fileUpload = require("express-fileupload");
-var ObjectId = require("mongodb").ObjectId;
+const mongoose = require('mongoose')
 
-const { MongoClient } = require("mongodb");
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.54hym.mongodb.net/${process.env.DB_NAME}?retryWrites=true&w=majority`;
 
 app.use(express.json());
@@ -15,130 +14,111 @@ app.use(cors());
 app.use(express.static("photography"));
 app.use(fileUpload());
 
-const client = new MongoClient(uri, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
+
+
+
+// user schema
+const serviceSchema = new mongoose.Schema({
+  title: String,
+  price: String,
+  description: String,
+  image: String
+})
+
+const Service = mongoose.model('Service', serviceSchema)
+
+mongoose.set('strictQuery', false)
+mongoose.connect(uri, function (err) {
+  if (err) {
+    console.log("err", err)
+  } else {
+    console.log("DB Connection established");
+  }
+})
+
+
+// app.post("/add-service", (req, res) => {
+//   const service = req.body;
+//   servicesCollection.insertOne(service).then((result) => res.send(result));
+// });
+
+app.get("/services", async (req, res) => {
+  try {
+    const services = await Service.find();
+    res.send(services)
+  } catch (err) {
+
+  }
 });
-client.connect((err) => {
-  console.log("connect err", err);
-  const servicesCollection = client.db("PhotixDB").collection("services");
-  const ordersCollection = client.db("PhotixDB").collection("orders");
-  const adminCollection = client.db("PhotixDB").collection("adminList");
-  // perform actions on the collection object
 
-  // express-fileupload
-  // app.post("/add-service", (req, res) => {
-  //   const file = req.files.image;
-  //   const price = req.body.price;
-  //   const title = req.body.title;
-  //   const description = req.body.description;
-  //   //   console.log(image, price, title, description);
-  //   const filePath = `${__dirname}/photography/${file.name}`;
-  //   file.mv(filePath, (err) => {
-  //     if (err) {
-  //       console.log(err);
-  //       res.status(500).send({ massage: "Failed to upload image!" });
-  //     }
-  //     const newImage = fs.readFileSync(filePath);
-  //     const encodedImage = newImage.toString("base64");
-  //     const image = {
-  //       contentType: file.mimetype,
-  //       size: file.size,
-  //       img: Buffer(encodedImage, "base64"),
-  //     };
-  //     servicesCollection
-  //       .insertOne({ title, price, description, image })
-  //       .then((result) => {
-  //         fs.remove(filePath, (err) => {
-  //           if (err) {
-  //             console.log(err);
-  //           }
-  //         });
-  //         res.send(result);
-  //         console.log(result);
-  //       });
-  //   });
-  // });
+// app.get("/services/:id", (req, res) => {
+//   const id = req.params.id;
+//   servicesCollection.find({ _id: ObjectId(id) }).toArray((err, service) => {
+//     res.send(service);
+//   });
+// });
 
-  app.post("/add-service", (req, res) => {
-    const service = req.body;
-    servicesCollection.insertOne(service).then((result) => res.send(result));
-  });
+// app.post("/take-order", (req, res) => {
+//   const data = req.body;
+//   ordersCollection.insertOne({ ...data }).then((result) => res.send(result));
+// });
 
-  app.get("/services", (req, res) => {
-    servicesCollection.find({}).toArray((err, service) => {
-      res.send(service);
-    });
-  });
+// app.post("/orders-by-each-user", (req, res) => {
+//   const email = req.body.email;
+//   ordersCollection
+//     .find({ logged_user_email: email })
+//     .toArray((err, orders) => res.send(orders));
+// });
 
-  app.get("/services/:id", (req, res) => {
-    const id = req.params.id;
-    servicesCollection.find({ _id: ObjectId(id) }).toArray((err, service) => {
-      res.send(service);
-    });
-  });
+// app.post("/orders", (req, res) => {
+//   const email = req.body.email;
+//   adminCollection.find({ email: email }).toArray((err, adminList) => {
+//     if (adminList.length > 0) {
+//       ordersCollection.find({}).toArray((err, orders) => res.send(orders));
+//     } else {
+//       res.send([]);
+//     }
+//   });
+// });
 
-  app.post("/take-order", (req, res) => {
-    const data = req.body;
-    ordersCollection.insertOne({ ...data }).then((result) => res.send(result));
-  });
+// app.post("/check-access", (req, res) => {
+//   const email = req.body.email;
+//   adminCollection
+//     .find({ email: email })
+//     .toArray((err, adminList) => res.send(adminList.length > 0));
+// });
 
-  app.post("/orders-by-each-user", (req, res) => {
-    const email = req.body.email;
-    ordersCollection
-      .find({ logged_user_email: email })
-      .toArray((err, orders) => res.send(orders));
-  });
+// app.patch("/update-order-status", (req, res) => {
+//   const { id, status } = req.body;
+//   ordersCollection
+//     .updateOne({ _id: ObjectId(id) }, { $set: { status: status } })
+//     .then((result) => res.send(result));
+// });
 
-  app.post("/orders", (req, res) => {
-    const email = req.body.email;
-    adminCollection.find({ email: email }).toArray((err, adminList) => {
-      if (adminList.length > 0) {
-        ordersCollection.find({}).toArray((err, orders) => res.send(orders));
-      } else {
-        res.send([]);
-      }
-    });
-  });
+// app.post("/manage-service", (req, res) => {
+//   const email = req.body.email;
+//   adminCollection.find({ email: email }).toArray((err, adminList) => {
+//     if (adminList.length > 0) {
+//       servicesCollection
+//         .find({})
+//         .toArray((err, services) => res.send(services));
+//     } else {
+//       res.send([]);
+//     }
+//   });
+// });
 
-  app.post("/check-access", (req, res) => {
-    const email = req.body.email;
-    adminCollection
-      .find({ email: email })
-      .toArray((err, adminList) => res.send(adminList.length > 0));
-  });
+// app.delete("/delete-service/:id", (req, res) => {
+//   const id = req.params.id;
+//   console.log("id", id);
+//   servicesCollection
+//     .deleteOne({ _id: ObjectId(id) })
+//     .then((result) => res.send(result));
+// });
 
-  app.patch("/update-order-status", (req, res) => {
-    const { id, status } = req.body;
-    ordersCollection
-      .updateOne({ _id: ObjectId(id) }, { $set: { status: status } })
-      .then((result) => res.send(result));
-  });
-
-  app.post("/manage-service", (req, res) => {
-    const email = req.body.email;
-    adminCollection.find({ email: email }).toArray((err, adminList) => {
-      if (adminList.length > 0) {
-        servicesCollection
-          .find({})
-          .toArray((err, services) => res.send(services));
-      } else {
-        res.send([]);
-      }
-    });
-  });
-
-  app.delete("/delete-service/:id", (req, res) => {
-    const id = req.params.id;
-    console.log("id", id);
-    servicesCollection
-      .deleteOne({ _id: ObjectId(id) })
-      .then((result) => res.send(result));
-  });
-});
 
 app.get("/", (req, res) => {
   res.send("photix server");
 });
 
-app.listen(process.env.PORT || port);
+app.listen(port);
